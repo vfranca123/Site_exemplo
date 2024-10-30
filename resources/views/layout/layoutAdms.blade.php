1710c6fab0dc57c05cb7e5129e32f3a044fe4931
Vitor <vitorferreirafranca@gmail.com>
2024-10-30 16:47:35 -0300

    organização front de adm

--- resources/views/layout/layoutAdms.blade.php
+++ resources/views/layout/layoutAdms.blade.php
@@ -7,13 +7,16 @@
     <script src="https://kit.fontawesome.com/cd911c24bf.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    
+    <link rel="stylesheet" href="/css/app.css">
+
+
     <title>Home Page Adiministrador</title>
 </head>
 <body>
-    <div class="d-flex" style="height: 100vh">
-        <div class="border">@include('shared.NavAdms')</div>
-        <div class="border">@yield('content')</div>
+    <div class="d-flex telaFull border" style="background-color:#F4F4F5" >
+        <div>@include('shared.NavAdms')</div>
+        <div>@yield('content')</div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
--- resources/views/shared/NavAdms.blade.php
+++ resources/views/shared/NavAdms.blade.php
@@ -1,6 +1,8 @@
 
-<ul class="nav flex-column">
-    <h1 class="m-3">Music</h1>
+<ul class="nav flex-column col-md-12">
+   
+    <img  class="col-md-6 m-3" src="storage/aplicação/logo.png" alt="">    
+    
     <li class="nav-item m-3">
         <a class="nav-link text-dark" aria-current="page" href="#">Loja</a>
     </li>
@@ -11,6 +13,6 @@
         <a class="nav-link text-dark" href="#">Vendas</a>
     </li>
     <li class="nav-item m-3">
-        <a class="nav-link " aria-disabled="true"><i class="fa-solid fa-right-from-bracket text-warning fs-1" style="cursor:pointer;"></i></a>
+        <a class="nav-link " aria-disabled="true"><i class="fa-solid fa-right-from-bracket text-warning fs-2" style="cursor:pointer;"></i></a>
     </li>
 </ul>
